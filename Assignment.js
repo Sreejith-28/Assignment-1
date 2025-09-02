0b9fe8010ab88712d54c30a3cdba4fb77bcde2ec
Sreejith-28 <sreejithsreeju2810@gmail.com>
2025-09-03 00:07:45 +0530

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,50 @@
+//1 ) Variables & Scope :
+
+var a = 5 ;
+console.log(a);
+
+var name = "Hello";
+// console.log(name); 
+var name ="World";
+console.log(name);
+
+let container = 100;
+console.log(container);
+
+// let Pens = 15;
+// console.log(Pens);
+let Pens = 20;
+console.log(Pens)
+
+const value ="Java Script";
+console.log(value)
+
+// 2 )Printing Statement :
+
+console.log("I am Learning JavaScript!")
+
+console.error("This is an error!")
+
+console.warn("This is a Warning!")
+
+alert("Welcome!")
+
+let Name = prompt("What's your Name");
+console.log(Name)
+
+// 3) Data Types 
+
+let myvariable = 25;
+console.log(typeof myvariable);
+
+let myString = "Hello";
+console.log(typeof myString);
+
+let myBoolean = true;
+console.log(typeof myBoolean);
+
+let myNull = null;
+console.log(typeof myNull);
+
+let myUndefined ;
+console.log(typeof myUndefined);
